68a3157c65f676e065156aeb15da5d5140c4f343
nicolasquinchia <nicolasquinchia@gmail.com>
2020-02-13 01:02:44 +0000

    0x02. C - Functions, nested loops, 11. Holberton School, 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,19 +12,25 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (x = n; x >= 97; x--)
+		for (x = n; x >= 98; x--)
 		{
+			if (x == 98)
+			printf("%d", x);
+			else
 			printf("%d, ", x);
 		}
-		printf("98\n");
+		putchar('\n');
 	}
 	else if (n < 98)
 	{
-		for (x = n; x <= 97; x++)
+		for (x = n; x <= 98; x++)
 		{
+			if (x == 98)
+			printf("%d", x);
+			else
 			printf("%d, ", x);
 		}
-		printf("98\n");
+		putchar('\n');
 	}
 	else
 	{
