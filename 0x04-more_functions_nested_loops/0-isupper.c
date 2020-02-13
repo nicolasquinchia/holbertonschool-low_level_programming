df944f0c96903ae7545ca84fd431790f9ac8ad69
nicolasquinchia <nicolasquinchia@gmail.com>
2020-02-13 18:14:12 +0000

    0x04. C - More functions, more nested loops, 0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * isupper - checks for uppercase character
+ * @c: ascii code for the character to check
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	int upc;
+
+	if (c <= 90 && c >= 65)
+	{
+		upc = 1;
+	}
+	else
+	{
+		upc = 0;
+	}
+	return (upc);
+}
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,7 @@
+#ifndef LIB
+#define LIB
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
