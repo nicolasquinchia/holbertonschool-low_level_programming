608536641a4e980c866177cba1d5659e977248a7
nicolasquinchia <nicolasquinchia@gmail.com>
2020-02-13 18:54:10 +0000

    0x04. C - More functions, more nested loops, 2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two integers
+ * @a: first integer to mult
+ * @b: second integer to mult
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (prod);
+}
