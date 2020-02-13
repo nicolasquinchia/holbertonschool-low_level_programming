d1354f2dfe7f1f6c71fba6f37ce0076eca43e357
nicolasquinchia <nicolasquinchia@gmail.com>
2020-02-13 00:48:35 +0000

    0x02. C - Functions, nested loops, 11. Holberton School, 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all num from n to 98
+ * @n: initial number 
+ *
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{	
+	int x;
+
+	if (n > 98)
+	{
+		for (x = n; x >= 97; x--)
+		{
+			printf("%d, ", x);
+		}
+		printf("98");
+		putchar('\n');
+	}
+	else if (n < 98) 
+	{
+		for (x = n; x <= 97; x++)
+		{
+			printf("%d, ", x);
+		}
+		printf("98");
+		putchar('\n');
+	}
+	else
+	{
+		printf("%d\n", n);
+	}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -13,5 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 void print_to_98(int n);
 int add(int a, int b);
+void print_to_98(int n);
 
 #endif
