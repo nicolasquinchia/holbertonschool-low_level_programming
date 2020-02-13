1f0c6b5adb2859b7ae4a5d5a1a169931c4878152
nicolasquinchia <nicolasquinchia@gmail.com>
2020-02-13 00:52:23 +0000

    0x02. C - Functions, nested loops, 11. Holberton School, 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 /**
  * print_to_98 - prints all num from n to 98
- * @n: initial number 
+ * @n: initial number
  *
  * Return: Always 0.
  */
 void print_to_98(int n)
-{	
+{
 	int x;
 
 	if (n > 98)
@@ -19,7 +19,7 @@ void print_to_98(int n)
 		printf("98");
 		putchar('\n');
 	}
-	else if (n < 98) 
+	else if (n < 98)
 	{
 		for (x = n; x <= 97; x++)
 		{
