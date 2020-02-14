599efea59bc5bf1b67283c00d7bc0f2a60863bee
nicolasquinchia <nicolasquinchia@gmail.com>
2020-02-14 00:21:31 +0000

    0x04. C - More functions, more nested loops, 8. You are so much sunshine in every square inch, add if

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,19 @@ void print_square(int size)
 	int x;
 	int y;
 
-	for (x = 0; x < size; x++)
+	if (size <= 0)
 	{
-		for (y = 0; y < size; y++)
-		{
-			_putchar(35);
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
 	}
 }
