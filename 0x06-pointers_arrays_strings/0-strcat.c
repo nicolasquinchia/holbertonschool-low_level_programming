87f8183ba00e1f987c7988f9702763412b70fe43
nicolasquinchia <nicolasquinchia@gmail.com>
2020-02-20 18:17:34 +0000

    0x06. C - More pointers, arrays and strings, 0. strcat, fix errors gives by betty, add null character

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,11 +16,11 @@ char *_strcat(char *dest, char *src)
 	for (c_dest = 0; dest[c_dest] != 0; c_dest++)
 	for (c_src = 0; src[c_src] != 0; c_src++)
 	i_src = 0;
-	for (i_dest = c_dest; i_dest <= (c_src + c_dest);)
+	for (i_dest = c_dest; i_dest < (c_src + c_dest); i_dest++)
 	{
 		dest[i_dest] = src[i_src];
 		i_src++;
-		i_dest++;
 	}
+	dest[c_src + c_dest] = 0;
 	return (dest);
 }
