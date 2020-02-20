4f1bae2d09e512af99eb2f82e9a9caff53e51a1a
nicolasquinchia <nicolasquinchia@gmail.com>
2020-02-20 22:28:33 +0000

    0x06. C - More pointers, arrays and strings, 2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * _strncat -  concatenates two strings.
+ * @src: array with the string source
+ * @dest: pointer where the text is append
+ * @n: num of bytes to be append from src
+ *
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int cont_d = 0;
+	int cont_s = 0;
+	int i;
+
+	while (dest[cont_d] != '\0')
+	{
+		cont_d++;
+	}
+	while (src[cont_s] != '\0' && cont_s < (n - 1))
+	{
+		cont_s++;
+	}
+	for (i = 0 ; i < cont_d + cont_s; i++)
+	{
+		if (i <= cont_s)
+		{
+			dest[i] = src[i];
+		}
+		else
+		{
+			dest[i] = dest[i];
+		}
+	}
+	dest[cont_d + cont_s] = '\0';
+	return (dest);
+}
