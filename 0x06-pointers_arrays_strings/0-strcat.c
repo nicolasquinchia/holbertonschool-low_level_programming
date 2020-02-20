48d8cc44fbe92ae21d75de103876afae2661771a
nicolasquinchia <nicolasquinchia@gmail.com>
2020-02-20 17:42:15 +0000

    0x06. C - More pointers, arrays and strings, 0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strcpy -  copies the string pointed to by src.
+ * @src: array with the string source
+ * @dest: pointer where the test is paste
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int c_src;
+	int c_dest;
+	int i_src;
+	int i_dest;
+
+	for (c_dest = 0; dest[c_dest] != 0; c_dest++)
+	for (c_src = 0; src[c_src] != 0; c_src++)
+	i_src = 0;
+	for (i_dest = c_dest; i_dest <= c_src + c_dest; i_dest++)
+	{
+		dest[i_dest] = src[i_src];
+		i_src++;
+	}
+	return (dest);
+}
