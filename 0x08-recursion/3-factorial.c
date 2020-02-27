e498094daa64a7b108a401d22b133b285316fd9a
nicolasquinchia <nicolasquinchia@gmail.com>
2020-02-27 00:45:10 +0000

    0x08. C - Recursion, 2. Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strangeand 3. You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *factorial - function that returns the factorial
+ *@n:integer to apply factorial
+ *
+ * Return: Always 0 (Success)
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
