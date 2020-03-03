d696884495980f0ad4b0bff3faefe87f94f9da30
nicolasquinchia <nicolasquinchia@gmail.com>
2020-03-03 18:46:00 +0000

    0x0B. C - malloc, free, 0x0B, 0. Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - create an array of chars, and initializes with specific char.
+ * @c: The character to print
+ * @size: The size of the array to create
+ *
+ * Return: pointer with the address of the array created .
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	p = malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	return (p);
+}
