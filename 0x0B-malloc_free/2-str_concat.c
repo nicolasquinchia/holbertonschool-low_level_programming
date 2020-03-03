119873f49c1d8e41c9a5769222bdceba2a050ec5
nicolasquinchia <nicolasquinchia@gmail.com>
2020-03-03 22:17:41 +0000

    0x0B. C - malloc, free, 0x0B, 2. He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * str_concat - concatenates two strings.
+ * @s1: The string #1 to concatenate.
+ * @s2: The string #2 to concatenate
+ *
+ * Return: pointer with the address of the array created .
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i_s1, i_s2;
+	char *p;
+	int i, j;
+
+	if (s1 == '\0')
+		s1 ="";
+	else if (s2 == '\0')
+		s2 ="";
+	for (i_s1 = 0; s1[i_s1] != '\0'; i_s1++)
+	{
+	}
+	for (i_s2 = 0; s2[i_s2] != '\0'; i_s2++)
+	{
+	}
+	p = malloc(((i_s1 + i_s2) * sizeof(char)) + 1);
+	for (i = 0; i < i_s1; i++)
+	{
+		p[i] = s1[i];
+	}
+	j = 0;
+	for (i = i_s1; i <= (i_s1 + i_s2); i++)
+	{
+		p[i] = s2[j];
+		j++;
+	}
+	return (p);	
+}
