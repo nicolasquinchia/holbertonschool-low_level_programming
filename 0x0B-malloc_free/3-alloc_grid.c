051c0fd1502655cc1ec9f2328b809c287c978e1e
nicolasquinchia <nicolasquinchia@gmail.com>
2020-03-04 21:34:06 +0000

    0x0B. C - malloc, free, 0x0B, 3. If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return ('\0');
-	p = malloc(sizeof(int *) + height);
+	p = malloc(sizeof(int *) * height);
 	if (p == '\0')
 		return ('\0');
 	for (i = 0; i < height; i++)
