4ef12e7eb3dfe6fa66c17443de33c154819bb917
nicolasquinchia <nicolasquinchia@gmail.com>
2020-03-05 00:40:00 +0000

    0x0B. C - malloc, free, 5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: size of av.
+ * @av: array of strings.
+ *
+ * Return: pointer with the address of the array created.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, x, y, n, m;
+	char *p;
+
+	if (ac == 0 || av == '\0')
+		return ('\0');
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			n++;
+		}
+	}
+	p = malloc(sizeof(char) * (n + ac + 1));
+	if (p == '\0')
+		return ('\0');
+	m = 0;
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[x][y] != '\0'; y++)
+		{
+			p[m] = av[x][y];
+			m++;
+		}
+		p[m] = '\n';
+		m++;
+	}
+	p[m] = '\0';
+	return (p);
+}
