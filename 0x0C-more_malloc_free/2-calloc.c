21074e8ef5bd9d9df76eca0c4205585a21bbdc32
nicolasquinchia <nicolasquinchia@gmail.com>
2020-03-05 20:42:02 +0000

    0x0C task 2, add < condition

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,12 +12,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *p;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
 		return ('\0');
 	p = malloc(nmemb * size);
 	if (p == '\0')
 		return ('\0');
-	for (i = 0; i <= (nmemb * size); i++)
+	for (i = 0; i < (nmemb * size); i++)
 	{
 		p[i] = 0;
 	}
