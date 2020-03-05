724fa6ccc3db552ba5c0be53f27ab6aa8627f42b
nicolasquinchia <nicolasquinchia@gmail.com>
2020-03-05 23:25:37 +0000

    0x0B, advanced task 4, realloc; change if new_size is >

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,10 +26,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (np);
 	}
 	op = ptr;
-	if (new_size < old_size || new_size > old_size)
+	if (new_size < old_size)
 	{
 		np = malloc(new_size);
 		for (i = 0; i < new_size; i++)
+			np[i] = op[i];
+	}
+	if (new_size > old_size)
+	{
+		np = malloc(new_size);
+		for (i = 0; i < old_size; i++)
 			np[i] = op[i];
 	}
 	free(ptr);
