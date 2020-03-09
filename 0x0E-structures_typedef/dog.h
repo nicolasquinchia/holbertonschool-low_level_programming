078096136142ff02e95fa0729c90e0d1c30c554d
nicolasquinchia <nicolasquinchia@gmail.com>
2020-03-09 22:12:11 +0000

    0x0E, def struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef Dog
 #define Dog
 
+/**
+ * struct dog - Dogs information
+ * @name: dog's name
+ * @age: age of the dog
+ * @owner: owner's name
+ *
+ * Description: General information abaout a dog
+ */
+
 struct dog
 {
 	char *name;
