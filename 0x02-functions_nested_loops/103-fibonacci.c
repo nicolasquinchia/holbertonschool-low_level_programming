1fe289dad71a30b9ac856edcb2ff0f2c032cc10a
nicolasquinchia <nicolasquinchia@gmail.com>
2020-04-02 04:57:39 +0000

    0x02 fibonacci numbers %2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long int sum = 0; 
+	unsigned long int p_num = 1;
+	unsigned long int c_num = 2;
+	unsigned long int temp = 0;
+
+	while (c_num <= 4000000)
+	{
+		if((c_num % 2) == 0)
+		{
+			sum = sum + c_num;
+		}
+		temp = c_num;
+		c_num = c_num + p_num;
+		p_num = temp;
+
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
