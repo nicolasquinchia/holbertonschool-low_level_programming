61dcf042a945a16b60cb1d906eb75f12cd99b058
nicolasquinchia <nicolasquinchia@gmail.com>
2020-04-02 01:04:31 +0000

    0x02, fibonacci, add temp variable

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,12 +9,14 @@ int main(void)
 	int i;
 	unsigned long int num_a = 0;
 	unsigned long int num_b = 1;
+	unsigned long int temp = 0;
 
 	i = 0;
 	while (i < 50)
 	{
-		num_b = num_a + num_b;
+		temp = num_a + num_b;
 		num_a = num_b;
+		num_b = temp;
 		printf("%lu", num_b);
 		if (i != 49)
 		{
