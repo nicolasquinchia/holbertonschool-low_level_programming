6aa49d700817611257a6583ef6ef0dd8133035c9
nicolasquinchia <nicolasquinchia@gmail.com>
2020-07-03 23:59:05 -0500

    0x1C, island perimeter python matrix

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This Module holds the funtion island perimetermeter
+    that calculates the perimetermeter of a grid(island)
+    """
+
+
+def island_perimetermeter(grid):
+    """This function Returns the perimetermeter of a grid(island)
+    simulated as a list of list (matrix) of 1 and 0
+
+    Args:
+        grid ([list]): llist of lists that represent the grid to check
+
+    Returns:
+        [int]: perimeter of the grid 1 represents terrain 0 water
+    """
+    perimeter = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                if y + 1 >= len(grid[x]) or grid[x][y + 1] == 0:
+                    perimeter += 1
+                if y - 1 < 0 or grid[x][y - 1] == 0:
+                    perimeter += 1
+                if x + 1 >= len(grid) or grid[x + 1][y] == 0:
+                    perimeter += 1
+                if x - 1 < 0 or grid[x - 1][y] == 0:
+                    perimeter += 1
+    return perimeter
